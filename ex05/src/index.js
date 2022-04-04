a2925deaaaf859e687a212a154af39359974037a
siriuska <you@example.com>
2022-04-04 18:03:45 +0200

    Understanding undefined value

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,16 +2,15 @@ var sum = 0;
 
 function addThree() {
   sum = sum + 3;
-  console.log("addThree: " + sum);
+  console.log("sum from addThree: " + sum);
 }
 
 function addFive() {
   sum = sum + 5;
-  console.log("addFive: " + sum);
+  console.log("sum from addFive: " + sum);
 }
-
-console.log(addThree());
-console.log(addFive());
+addThree();
+addFive();
 
 
 module.exports = {
